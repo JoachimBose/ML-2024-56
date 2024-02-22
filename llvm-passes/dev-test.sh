73a77b5776ce23cb6e776296c88935ddf3d5be0d
joachimBose <joachimbose48@gmail.com>
2024-02-22 17:46:09 +0100

    made: dummy pass now runs

---
+++ llvm-passes/dev-test.sh
@@ -0,0 +1,1 @@
+opt-17 -load-pass-plugin ./build/libFeatExtr.so -passes=feat-extr ./2mm.ll -S
--- setup.sh
+++ setup.sh
@@ -5,4 +5,5 @@ echo deb-src http://apt.llvm.org/focal/ llvm-toolchain-focal-17 main | sudo tee 
 wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
 sudo apt update && sudo apt upgrade
 sudo apt-get install libllvm17 llvm-17 llvm-17-dev llvm-17-doc llvm-17-examples\
-    llvm-17-runtime libedit-dev libzstd-dev libcurl4-gnutls-dev libstdc++-12-dev
+    llvm-17-runtime libedit-dev libzstd-dev libcurl4-gnutls-dev\
+    libstdc++-12-dev clang-17
