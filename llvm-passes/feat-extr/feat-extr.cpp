3e34d7fe982ecf79d22a3b68fc54002fb3cfbe60
joachimBose <joachimbose48@gmail.com>
2024-03-04 15:08:20 +0000

    Made: loopfeature framework

--- llvm-passes/feat-extr/feat-extr.cpp
+++ llvm-passes/feat-extr/feat-extr.cpp
@@ -1,21 +1,3 @@
-//=============================================================================
-// FILE:
-//    HelloWorld.cpp
-//
-// DESCRIPTION:
-//    Visits all functions in a module, prints their names and the number of
-//    arguments via stderr. Strictly speaking, this is an analysis pass (i.e.
-//    the functions are not modified). However, in order to keep things simple
-//    there's no 'print' method here (every analysis pass should implement it).
-//
-// USAGE:
-//    New PM
-//      opt -load-pass-plugin=libHelloWorld.dylib -passes="hello-world" `\`
-//        -disable-output <input-llvm-file>
-//
-//
-// License: MIT
-//=============================================================================
 #include <llvm/IR/LegacyPassManager.h>
 #include <llvm/Passes/PassBuilder.h>
 #include <llvm/Passes/PassPlugin.h>
@@ -23,57 +5,231 @@
 
 using namespace llvm;
 
-//-----------------------------------------------------------------------------
-// HelloWorld implementation
-//-----------------------------------------------------------------------------
-// No need to expose the internals of the pass to the outside world - keep
-// everything in an anonymous namespace.
-namespace {
 
-// This method implements what the pass does
-void visitor(Function &F) {
-    errs() << "(feat-extr) Hello from: "<< F.getName() << "\n";
-    errs() << "(feat-extr)   number of arguments: " << F.arg_size() << "\n";
-}
+/**
+Features:
+ - N-basic blocks
+ - Average instructions per basic block
+ - N-Conditional jumps
+ - N-loops
+ -
+*/
+namespace
+{
+    class FunctionFeature
+    {
+        public:
+        virtual void runOnFunction(Function &function){}
+        virtual int getResult() { errs() << " baseclass "; return -1; }
+    };
 
-// New PM implementation
-struct HelloWorld : PassInfoMixin<HelloWorld> {
-  // Main entry point, takes IR unit to run the pass on (&F) and the
-  // corresponding pass manager (to be queried if need be)
-  PreservedAnalyses run(Function &F, FunctionAnalysisManager &) {
-    visitor(F);
-    return PreservedAnalyses::all();
-  }
+    // class LoopFeature{
+    //     private:
+    //     public:
+    //     virtual void runOnLoop(Loop& loop){}
+    //     virtual int getResult() { errs() << " baseloop "; return -1; }
+    // };
 
-  // Without isRequired returning true, this pass will be skipped for functions
-  // decorated with the optnone LLVM attribute. Note that clang -O0 decorates
-  // all functions with optnone.
-  static bool isRequired() { return true; }
-};
+    // class LoopCounter : public LoopFeature
+    // {
+    // private:
+    //     int nLoops;
+    // public:
+    //     LoopCounter()
+    //     {
+    //         nLoops = 0;
+    //     }
+    //     void runOnLoop(Loop& loop){
+    //         nLoops ++;
+    //     }
+    //     int getResult(){
+    //         return nLoops;
+    //     }
+    // };
+
+    class BasicBlockCounter : public FunctionFeature
+    {
+    private:
+        int nBasicBlocks;
+
+    public:
+        void runOnFunction(Function &function)
+        {
+            for (BasicBlock &basicBlock : function)
+            {
+                nBasicBlocks++;
+            }
+        }
+        BasicBlockCounter()
+        {
+            nBasicBlocks = 0;
+        }
+        int getResult() { errs() << " bbcount "; return nBasicBlocks; }
+    };
+
+    class InstructionCounter : public FunctionFeature
+    {
+    private:
+        int nInstructions = 0;
+
+    public:
+        void runOnFunction(Function &function)
+        {
+            for (BasicBlock &basicBlock : function)
+            {
+                for (Instruction &instruction : basicBlock)
+                {
+                    nInstructions ++;
+                }
+            }
+        }
+        InstructionCounter()
+        {
+            nInstructions = 0;
+        }
+        int getResult() { return nInstructions; }
+    };
+
+    class ConditionalJMPCounter : public FunctionFeature
+    {
+    private:
+        int nConditionalJMP;
+    public:
+        ConditionalJMPCounter(){
+            nConditionalJMP = 0;
+        }
+        void runOnFunction(Function &function){
+            for (BasicBlock& basicBlock : function)
+            {
+                for (Instruction& instruction : basicBlock)
+                {
+                    if (auto branchInst = dyn_cast<BranchInst>(&instruction))
+                    {
+                        nConditionalJMP += branchInst->isConditional()? 1 : 0;
+                    }
+                }
+            }
+        }
+        int getResult() { return nConditionalJMP; }
+    };
+
+    /*Gregs voice is ghosting through my head :'(
+        , AND IT IS NOT EVEN A REAL FACTORY! >:'(
+    */
+    class FeatureFactory
+    {
+    public:
+        SmallVector<FunctionFeature*> functionFeatures;
+        // SmallVector<LoopFeature*> loopFeatures;
+
+        FeatureFactory()
+        {
+            functionFeatures.push_back(new ConditionalJMPCounter());
+            functionFeatures.push_back(new InstructionCounter());
+            functionFeatures.push_back(new BasicBlockCounter());
+
+            // loopFeatures.push_back(new LoopCounter());
+        }
+        ~FeatureFactory(){
+            
+            for (size_t i = 0; i < functionFeatures.size(); i++)
+            {
+                delete functionFeatures[i];
+            }
+            // for (size_t i = 0; i < loopFeatures.size(); i++)
+            // {
+            //     delete loopFeatures[i];
+            // }
+        }
+    };
+    
+    
+    FeatureFactory features = FeatureFactory();
+
+    // New PM implementation
+    struct HelloWorld : PassInfoMixin<HelloWorld>
+    {
+        // Main entry point, takes IR unit to run the pass on (&F) and the
+        // corresponding pass manager (to be queried if need be)
+
+        void runOnFunction(Function& function)
+        {
+            for (size_t i = 0; i < features.functionFeatures.size(); i++)
+            {
+                features.functionFeatures[i]->runOnFunction(function);
+            }
+        }
+        void exportResults(){
+            for (size_t i = 0; i < features.functionFeatures.size(); i++)
+            {
+                errs() << "funcFeat " << i << " " << features.functionFeatures[i]->getResult() << "\n";
+            }
+            // for (size_t i = 0; i < features.loopFeatures.size(); i++)
+            // {
+            //     errs() << "loopFeat " << i << " " << features.functionFeatures[i]->getResult() << "\n";
+            // }
+        }
+
+        // PreservedAnalyses run(Loop &L, LoopAnalysisManager &AM,
+        //     LoopStandardAnalysisResults &AR,
+        //     LPMUpdater &) {
+        //     for (size_t i = 0; i < features.loopFeatures.size(); i++)
+        //     {
+        //         features.loopFeatures[i]->runOnLoop(L);
+        //     }
+        //     errs() << "loop pass finished\n";
+        //     return PreservedAnalyses::all();
+        // }
+
+        // void getAnalysisUsage(AnalysisUsage &analysisUsage){
+        //     analysisUsage.addPreserved<LoopAnalysis>();
+        //     getLoopAnalysisUsage(analysisUsage);
+        // }
+
+        PreservedAnalyses run (Module &module, ModuleAnalysisManager &AM){
+            // LoopAnalysis::Result analysis = AM.getResult<LoopAnalysis>(module);
+            for (Function &function : module)
+            {
+                runOnFunction(function);
+            }
+            exportResults();
+            return PreservedAnalyses::all();
+        }
+
+        // Without isRequired returning true, this pass will be skipped for functions
+        // decorated with the optnone LLVM attribute. Note that clang -O0 decorates
+        // all functions with optnone.
+        static bool isRequired() { return true; }
+    };
 } // namespace
 
 //-----------------------------------------------------------------------------
 // New PM Registration
 //-----------------------------------------------------------------------------
-llvm::PassPluginLibraryInfo getHelloWorldPluginInfo() {
-  return {LLVM_PLUGIN_API_VERSION, "feat-extr", LLVM_VERSION_STRING,
-          [](PassBuilder &PB) {
-            PB.registerPipelineParsingCallback(
-                [](StringRef Name, FunctionPassManager &FPM,
-                   ArrayRef<PassBuilder::PipelineElement>) {
-                  if (Name == "feat-extr") {
-                    FPM.addPass(HelloWorld());
-                    return true;
-                  }
-                  return false;
-                });
-          }};
+llvm::PassPluginLibraryInfo getHelloWorldPluginInfo()
+{
+    return {LLVM_PLUGIN_API_VERSION, "feat-extr", LLVM_VERSION_STRING,
+            [](PassBuilder &PB)
+            {
+                PB.registerPipelineParsingCallback(
+                    [](StringRef Name, ModulePassManager &FPM,
+                       ArrayRef<PassBuilder::PipelineElement>)
+                    {
+                        if (Name == "feat-extr")
+                        {
+                            FPM.addPass(HelloWorld());
+                            return true;
+                        }
+                        return false;
+                    });
+            }};
 }
 
 // This is the core interface for pass plugins. It guarantees that 'opt' will
 // be able to recognize HelloWorld when added to the pass pipeline on the
 // command line, i.e. via '-passes=hello-world'
 extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo
-llvmGetPassPluginInfo() {
-  return getHelloWorldPluginInfo();
+llvmGetPassPluginInfo()
+{
+    return getHelloWorldPluginInfo();
 }
