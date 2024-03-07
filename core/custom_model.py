fe96caaa6a405099f9f0105cf31fa6496cb09a6f
ricardo <rmohamedhoesein3@gmail.com>
2024-03-07 18:04:05 +0000

    Move the model and neural net to the core folder.

--- core/custom_model.py
+++ core/custom_model.py
@@ -1,15 +1,22 @@
 import os
 os.environ["KERAS_BACKEND"] = "torch"
 import torch
-import numpy as np
-from keras import Model
+import keras
+import subprocess
 
-class CustomModel(Model):
+test = "test"
+
+class CustomModel(keras.Model):
+    def __init__(self, test_names, *args, **kwargs):
+        self.test_names = test_names
+        super().__init__(*args, **kwargs)
+
     def train_step(self, data):
         # Unpack the data. Its structure depends on your model and
         # on what you pass to `fit()`.
         x, y = data
-
+        # x: Name, features
+        # y: target-size
         # Call torch.nn.Module.zero_grad() to clear the leftover gradients
         # for the weights from the previous train step.
         self.zero_grad()
@@ -18,11 +25,32 @@ class CustomModel(Model):
         y_pred = self(x, training=True)  # Forward pass
         print(x)
         print(y_pred)
+        true_y_pred = []
+        for index, t in enumerate(y_pred):
+            test_name = self.test_names[index]
+            rounded = list(map(lambda x : "1" if x > 0.5 else "0", t))
+            print("============================")
+            print(index)
+            print(test_name)
+            print(t)
+            print(rounded)
+            print(''.join(rounded))
+            print(["python3", "./compile.py", test_name, "".join(rounded)])
+            process = subprocess.run(
+                ["python3", "./compile.py", test_name, "".join(rounded)],
+                check=True,
+                capture_output=True,
+                text=True
+            )
+            text = process.stdout
+            size = int(text.split(": ")[1])
+            true_y_pred.append(size)
+
         #TODO y_pred are the determined optimisations in our case, and we should call clang with them.
         #This is for all the x passed in, so if we use 10 application,
         #then x is an array with 10 elements each of which is an array containing all the features in the input layer,
         #y is an array with 10 elements each of which contains the output for each of the values in the nodes in the output layer.
-        loss = self.compute_loss(y=y, y_pred=y_pred)
+        loss = self.compute_loss(y=y, y_pred=true_y_pred)
         
 
         # Call torch.Tensor.backward() on the loss to compute gradients
---
+++ core/neural_net.py
@@ -0,0 +1,30 @@
+import os
+os.environ["KERAS_BACKEND"] = "torch"
+from keras import Input, layers
+from custom_model import CustomModel
+from pandas import read_csv
+
+source_location = "../source.csv"
+input_count = 10
+output_count = 10
+target = "target-size"
+test = "test"
+
+data = read_csv(source_location)
+y_data = data[target].values.astype("int32")
+test_names = data[test].values
+x_data = data.drop([target, "test"], axis=1).values
+
+# https://keras.io/api/layers/
+# https://keras.io/api/layers/core_layers/
+inputs = Input(shape=(input_count,))
+x = layers.Dense(64, activation="relu")(inputs)
+x = layers.Dense(64, activation="relu")(x)
+outputs = layers.Dense(output_count, activation="sigmoid")(x)
+
+model = CustomModel(test_names, inputs=inputs, outputs=outputs, name="dummy")
+#https://keras.io/api/optimizers/
+#https://keras.io/api/losses/
+model.compile(optimizer="adam", loss="mse")
+
+model.fit(x_data, y_data, epochs=3)
